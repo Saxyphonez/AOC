89ec66310b89ab51045570b13e2f5349d955ed23
Aditya Revat <adi.revat@cmrsurgical.com>
2022-11-24 16:30:40 +0000

    adding boiler plate script and skeleton python code. Still WIP need to actually make the script do stuff

---
+++ 2021/boilerplate.py
@@ -0,0 +1,24 @@
+try:
+    import logging
+    import os
+
+except:
+    print("Imports failed")
+
+YEAR = 2021
+
+def main():
+    #take in day number
+    #create folder/ folder structure   
+    #create input.txt
+    #create 2x python files with basic code in #where does the code come from #write the day on top?
+
+    print("done")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+
+    except KeyboardInterrupt:
+        print("KB interrupt detected")

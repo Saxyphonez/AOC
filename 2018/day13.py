5ee704f9069f335bdf590d3be8c84f57246d3801
Aditya Revat <adi.revat@cmrsurgical.com>
2022-11-28 17:49:04 +0000

    update, WIP

--- 2018/day13.py
+++ 2018/day13.py
@@ -1,11 +1,57 @@
 try:
     import logging
     import os
+    from enum import Enum
 
 except:
     print("Imports failed")
 
 TEST = True
+
+class NextMove(Enum):
+    LEFT = 0
+    STRAIGHT = 1
+    RIGHT = 2
+    NOTHING = 4
+
+class Direction(Enum):
+    LEFT = 0
+    UP = 1
+    RIGHT = 2
+    DOWN = 4
+
+class Cart:
+
+    #Class variables
+    next_move = NextMove.NOTHING #for intersections
+    previous_move = NextMove.NOTHING
+
+    current_direction = None
+
+    def __init__(self, direction, x, y):
+        self.current_direction = direction
+        self.x= x #starting state
+        self.y = y
+
+    def calculate_next_move(self):
+        #set next move here 
+        #move current move to previous move
+        pass
+
+    def set_location(self, x, y):
+        pass
+
+    def get_location(self, x, y):
+        return self.x, self.y #where am I rn
+
+    def next_direction(self ):
+        #for intersections
+        pass
+
+    def turn_corner(self, which_way):
+        #for turning the corner
+        pass
+
 
 if TEST:
     input_filename = "test_input.txt"
@@ -16,7 +62,6 @@ def get_input():
     input = []
 
     input_filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), input_filename)
-    #print(input_filepath)
 
     with open(input_filepath,'r') as f:
         input = f.readlines()

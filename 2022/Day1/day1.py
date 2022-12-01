b876f66cd35f8e9b440e195f00082ed806fc2221
Aditya Revat <adi.revat@cmrsurgical.com>
2022-12-01 09:16:18 +0000

    day1 donme

--- 2022/Day1/day1.py
+++ 2022/Day1/day1.py
@@ -5,7 +5,7 @@ try:
 except:
     print("Imports failed")
 
-TEST = True
+TEST = not True
 
 if TEST:
     input_filename = "test_input.txt"
@@ -14,19 +14,41 @@ else:
 
 def get_input():
     input = []
+    buffer = []
+    output =[]
 
     input_filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), input_filename)
     #print(input_filepath)
 
     with open(input_filepath,'r') as f:
         input = f.readlines()
+            
+    for i, value in enumerate(input):
+        if value == '\n':
+            output.append(buffer.copy())
+            buffer.clear()
+        else:
+            num = int(value.strip())
+            buffer.append(num)
 
-    #return input
+    output.append(list(buffer))
+    print(output)
+    return output
 
 def main():
+    each_elf = get_input()
+    calories = []
 
+    for i, calories_list in enumerate(each_elf):
+        total_for_elf = sum(calories_list)
+        calories.append(total_for_elf)
 
-    print("done")
+    #print(calories)
+    max_value = max(calories)
+    index = calories.index(max_value)
+
+    print(index)
+    print(max_value)
 
 
 if __name__ == "__main__":

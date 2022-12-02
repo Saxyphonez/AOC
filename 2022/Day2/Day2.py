49ec3f3ab91156b2785ada66a434b1148f30997f
Adi Revat <adirevat@Adis-MacBook-Air.local>
2022-12-02 07:21:53 +0000

    finished day 2

--- 2022/Day2/Day2.py
+++ 2022/Day2/Day2.py
@@ -5,7 +5,16 @@ try:
 except:
     print("Imports failed")
 
-TEST = True
+TEST = not True
+
+
+DRAW = 3
+WIN = 6
+LOSE = 0
+
+ROCK = 1
+PAPER = 2
+SCIS = 3
 
 if TEST:
     input_filename = "test_input.txt"
@@ -16,18 +25,80 @@ def get_input():
     input = []
 
     input_filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), input_filename)
-    #print(input_filepath)
+    print(input_filepath)
 
     with open(input_filepath,'r') as f:
         input = f.readlines()
 
-    #return input
+    buffer = []
+
+    for i, val in enumerate(input):
+        buffer.append(val.strip().split(" "))
+
+
+    return buffer
+
+def score_for_opt(throw):
+    match throw:
+        case 'X':
+            return ROCK
+        case 'Y':
+            return PAPER
+        case 'Z':
+            return SCIS
+
+def do_turn(throws):
+    score = 0
+    opponent = throws[0]
+    player = throws[1]
+
+    #A Rock X = 1 pt
+    #B Paper Y = 2 pt
+    #C Scissors Z = 3 pt
+    # win = 6pt
+    # draw = 3 pt
+    # lose = 0 pt
+
+    if (opponent == 'A' and player == 'X') or (opponent == 'B' and player == 'Y') or (opponent == 'C' and player == 'Z'):
+        #draw
+        score = DRAW
+        match player:
+            case 'X':
+                score+=1
+            case 'Y':
+                score+=2
+            case 'Z':
+                score+=3
+        return score
+    
+    if opponent == 'A':
+        if player == 'Y':
+            score = WIN + PAPER
+        else:
+            score = score_for_opt(player)
+
+    elif opponent == 'B':
+        if player == 'Z':
+            score = WIN + SCIS
+        else:
+            score = score_for_opt(player)
+
+    elif opponent == 'C':
+        if player == 'X':
+            score = WIN + ROCK
+        else:
+            score = score_for_opt(player)
+
+    return score
 
 def main():
 
+    input = get_input()
+    score = 0
 
-    print("done")
-
+    for i, val in enumerate(input):
+        score += do_turn(val)
+    print(score)
 
 if __name__ == "__main__":
     try:
--- 2022/Day2/Day2_2.py
+++ 2022/Day2/Day2_2.py
@@ -5,7 +5,16 @@ try:
 except:
     print("Imports failed")
 
-TEST = True
+TEST = not True
+
+
+DRAW = 3
+WIN = 6
+LOSE = 0
+
+ROCK = 1
+PAPER = 2
+SCIS = 3
 
 if TEST:
     input_filename = "test_input.txt"
@@ -16,18 +25,81 @@ def get_input():
     input = []
 
     input_filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), input_filename)
-    #print(input_filepath)
+    print(input_filepath)
 
     with open(input_filepath,'r') as f:
         input = f.readlines()
 
-    #return input
+    buffer = []
+
+    for i, val in enumerate(input):
+        buffer.append(val.strip().split(" "))
+
+
+    return buffer
+
+def move_for_outcome(opponent, outcome):
+    if outcome == WIN:
+        match opponent:
+            case 'A':
+                return PAPER
+            case 'B':
+                return SCIS
+            case 'C':
+                return ROCK
+
+    elif outcome == DRAW:
+        match opponent:
+            case 'A':
+                return ROCK
+            case 'B':
+                return PAPER
+            case 'C':
+                return SCIS 
+
+    elif outcome == LOSE:
+        match opponent:
+            case 'A':
+                return SCIS
+            case 'B':
+                return ROCK
+            case 'C':
+                return PAPER
+
+
+
+def do_turn(throws):
+    score = 0
+    opponent = throws[0]
+    player = throws[1]
+
+    #A Rock = 1 pt
+    #B Paper = 2 pt
+    #C Scissors = 3 pt
+    # Z win = 6pt
+    # Y draw = 3 pt
+    # Xlose = 0 pt
+    match player:
+        case 'Z':
+            score = WIN + move_for_outcome(opponent, WIN)
+        
+        case 'Y':
+            score = DRAW + move_for_outcome(opponent, DRAW)
+        
+        case 'X':
+            score = LOSE + move_for_outcome(opponent, LOSE)
+
+    
+    return score
 
 def main():
 
+    input = get_input()
+    score = 0
 
-    print("done")
-
+    for i, val in enumerate(input):
+        score += do_turn(val)
+    print(score)
 
 if __name__ == "__main__":
     try:

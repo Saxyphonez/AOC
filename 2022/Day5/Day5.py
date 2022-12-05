609a647d270da8959e6d6af718f5d06bca760378
Aditya Revat <adi.revat@cmrsurgical.com>
2022-12-05 11:27:31 +0000

    Cleans stack of crates into something nicer - need to turn this into reparate stacks and then start doing the moves

--- 2022/Day5/Day5.py
+++ 2022/Day5/Day5.py
@@ -1,19 +1,26 @@
 try:
     import logging
     import os
+    import pandas as pd
+    import re
+    import numpy as np
 
 except:
     print("Imports failed")
 
 TEST = True
 
+
 if TEST:
     input_filename = "test_input.txt"
+    NUM_OF_STACK = 3
 else:
     input_filename = "input.txt"
+    NUM_OF_STACK = 9
 
 def get_input():
     input = []
+    buffer = []
 
     input_filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), input_filename)
     #print(input_filepath)
@@ -21,11 +28,55 @@ def get_input():
     with open(input_filepath,'r') as f:
         input = f.readlines()
 
-    #return input
+    break_index = input.index("\n")
+
+    stacks_raw = input[0:break_index-1]
+    moves_raw = input[break_index+1:]
+
+    moves = []
+    for i, value in enumerate(moves_raw):
+        tmp = re.findall(r'\b\d+\b', value)
+        tmp2 = [int(x) for x in tmp]
+        moves.append(tmp2.copy())
+        tmp2.clear()
+
+
+    return stacks_raw, moves
+
+
+def make_stacks(raw_input):
+    
+
+    for i, value in enumerate(raw_input):
+        raw_input[i] = value.replace("\n", " ")
+
+    clean_stack = clean_stack_items(raw_input)
+    #take clean_stack and split into separate columns
+
+    return stack
+
+def clean_stack_items(dirty):
+    clean = []
+    length = int(len(dirty[0])/4)
+
+    for i, value in enumerate(dirty):
+        for j in range(length):
+            tmp = value[4*j:(4*j)+4]
+
+            if re.match(r"\s{4}",tmp):
+                clean.append(" ")
+
+            elif re.match(r"\[(.*?)\]",tmp):
+                letter = re.match(r"\[(.*?)\]",tmp).string
+                clean.append(letter.strip())
+
+    return clean
 
 def main():
 
-
+    stacks_raw, moves = get_input() 
+    stack = make_stacks(stacks_raw)
+    #moves = [from stack num, amount, to stack num]
     print("done")
 
 

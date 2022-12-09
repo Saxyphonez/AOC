1c60b3e2fd3c4a76b0eb5ddebec614874a2988ab
Adi Revat <adirevat@Adi-MacBook-Air>
2022-12-09 06:53:38 +0000

    barebones and no logic

--- 2022/Day9/Day9.py
+++ 2022/Day9/Day9.py
@@ -6,6 +6,33 @@ except:
     print("Imports failed")
 
 TEST = True
+BOARD_L = 10
+BOARD_W = 10
+"""
+input parsing for the moves
+
+func for tail next move
+
+func for move execution
+
+rope_end class:
+    name string
+    x,y locations
+
+2d array for the board
+"""
+
+class RopeEnd():
+
+
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = int(x)
+        self.y = int(y)
+
+    def __repr__(self):
+        return str(self.name)
+
 
 if TEST:
     input_filename = "test_input.txt"
@@ -21,10 +48,27 @@ def get_input():
     with open(input_filepath,'r') as f:
         input = f.readlines()
 
-    #return input
+    input = [x.strip().split(" ") for x in input] 
+    return input
 
 def main():
+    head = RopeEnd("H", 0, 0)
+    tail = RopeEnd("T", 0, 0)
+    board = []
+    moves = get_input()
 
+    tmp = list((0 for element in range(BOARD_W)))
+    for i in range(BOARD_L):
+        board.append(tmp.copy())
+
+    board[BOARD_L-1][0] = 1 
+    """
+    for i, move in enumerate(moves):
+        move head()
+        calculate & do move tail()
+            update board with tail visits()
+    
+    """
 
     print("done")
 

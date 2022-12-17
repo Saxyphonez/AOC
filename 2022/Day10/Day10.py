a3f404d2b76bdb9a42b2cb8ab7c58e471c470518
Adi Revat <adirevat@Adi-MacBook-Air>
2022-12-17 10:34:21 +0000

    finished day 10

--- 2022/Day10/Day10.py
+++ 2022/Day10/Day10.py
@@ -1,11 +1,13 @@
 try:
     import logging
     import os
+    import re
 
 except:
     print("Imports failed")
 
-TEST = True
+TEST = not True
+
 
 if TEST:
     input_filename = "test_input.txt"
@@ -21,12 +23,46 @@ def get_input():
     with open(input_filepath,'r') as f:
         input = f.readlines()
 
-    #return input
+    input = [x.strip() for x in input]
+
+    return input
+
+
+def check_cycle_count(clk_cnt, reg_X):
+
+    if clk_cnt == 20 or (clk_cnt-20)%40 == 0:
+        print("Clk count = " + str(clk_cnt) + ", X = " + str(reg_X))
+        return clk_cnt*reg_X
+
+    else:
+        return 0
+
+    
 
 def main():
+    instructions = get_input()
+    reg_X = 1
+    clk = 0
+    sum = 0
 
+    for i, op in enumerate(instructions):
 
-    print("done")
+        if op == 'noop':
+            clk += 1
+            sum += check_cycle_count(clk, reg_X)
+            continue
+
+        else:
+            op_split = op.split(" ")
+            clk += 1
+            sum +=check_cycle_count(clk, reg_X)   
+            clk += 1
+            sum +=check_cycle_count(clk, reg_X)
+            reg_X += int(op_split[1])
+
+    #print(reg_X)
+
+    print(sum)
 
 
 if __name__ == "__main__":
--- 2022/Day10/Day10_2.py
+++ 2022/Day10/Day10_2.py
@@ -1,11 +1,13 @@
 try:
     import logging
     import os
+    import re
 
 except:
     print("Imports failed")
 
-TEST = True
+TEST = not True
+
 
 if TEST:
     input_filename = "test_input.txt"
@@ -21,13 +23,65 @@ def get_input():
     with open(input_filepath,'r') as f:
         input = f.readlines()
 
-    #return input
+    input = [x.strip() for x in input]
+
+    return input
+
+
+def check_cycle_count(clk_cnt, reg_X):
+
+    if clk_cnt == 20 or (clk_cnt-20)%40 == 0:
+        #print("Clk count = " + str(clk_cnt) + ", X = " + str(reg_X))
+        return clk_cnt*reg_X
+
+    else:
+        return 0
+
+    
 
 def main():
+    instructions = get_input()
+    reg_X = 1
+    clk = 0
+    sum = 0
+    crt = []
+
+    for i, op in enumerate(instructions):
+
+        if op == 'noop':
+            if clk%40 == reg_X - 1 or clk%40 == reg_X or clk%40 == reg_X +1:
+                crt.append("#")
+            else:
+                crt.append(".")
+            clk += 1
+            sum += check_cycle_count(clk, reg_X)
+            continue
+
+        else:
+            op_split = op.split(" ")
+            
+            if clk%40 == reg_X - 1 or clk%40 == reg_X or clk%40 == reg_X +1:
+                crt.append("#")
+            else:
+                crt.append(".")
+            clk += 1
+            sum +=check_cycle_count(clk, reg_X)   
+            
+            if clk%40 == reg_X - 1 or clk%40 == reg_X or clk%40 == reg_X +1:
+                crt.append("#")
+            else:
+                crt.append(".")
+            clk += 1
+            sum +=check_cycle_count(clk, reg_X)
+            reg_X += int(op_split[1])
 
 
-    print("done")
+    for i, px in enumerate(crt):
+        if i%40 == 0:
+            print(" ")
 
+        print(px, end="")
+    
 
 if __name__ == "__main__":
     try:

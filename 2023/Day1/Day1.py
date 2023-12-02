1e63632a7c6df5e273b64869b2dabb2cd4d1c028
aar1g17 <aar1g17@soton.ac.uk>
2023-12-02 19:58:12 +0000

    Complete day1 part1

--- 2023/Day1/Day1.py
+++ 2023/Day1/Day1.py
@@ -5,7 +5,7 @@ try:
 except:
     print("Imports failed")
 
-TEST = True
+TEST = not True
 
 if TEST:
     input_filename = "test_input.txt"
@@ -21,11 +21,39 @@ def get_input():
     with open(input_filepath,'r') as f:
         input = f.readlines()
 
-    #return input
+    input = [line.strip() for line in input]
+
+    return input
+
+def get_digits(line):
+    digits = []
+
+    for char in list(line):
+        if char.isdigit():
+            digits.append(char)
+        else:
+            continue
+
+    if len(digits) == 1:
+        digits.append(digits[0])
+    
+    return digits[0]+digits[-1]
+
+
 
 def main():
+    input = get_input()
 
+    important_digits = []
+    for line in input:
+        important_digits.append(get_digits(line))
 
+    #print(important_digits)
+    total = 0
+    for number in important_digits:
+        total+= int(number)
+
+    print(total)
     print("done")
 
 

4e2caec16a89a6674dccccf370974bcfe32bcd33
aar1g17 <aar1g17@soton.ac.uk>
2023-12-03 19:53:12 +0000

    Complete part 2. Very dirty tuple flattening used cos Idk regex

--- 2023/Day1/Day1_2.py
+++ 2023/Day1/Day1_2.py
@@ -1,11 +1,24 @@
 try:
     import logging
     import os
+    import re
 
 except:
     print("Imports failed")
 
-TEST = True
+TEST =  not True
+
+TEXT_NUMBERS = {
+    "one":'1',
+    "two":'2',
+    "three":'3',
+    "four":'4',
+    "five":'5',
+    "six":'6',
+    "seven":'7',
+    "eight":'8',
+    "nine":'9',
+}
 
 if TEST:
     input_filename = "test_input.txt"
@@ -21,11 +34,48 @@ def get_input():
     with open(input_filepath,'r') as f:
         input = f.readlines()
 
-    #return input
+    input = [line.strip() for line in input]
+
+    return input
+
+def get_digits(line):
+    digits = []
+
+
+    pattern = re.compile(r"(?:(\d)|(?=(one|two|three|four|five|six|seven|eight|nine)))")
+    #pattern = re.compile(r'\d')
+    regex_numbers = re.findall(pattern, line)
+    all_numbers= ["".join(x) for x in regex_numbers]
+    #print(all_numbers) 
+
+    for number in all_numbers:
+            if number.isdigit():
+                digits.append(number)
+            else:
+                digits.append(TEXT_NUMBERS[number])
+
+    if len(digits) == 1:
+        digits.append(digits[0])
+    
+    return digits[0]+digits[-1]
+
+
 
 def main():
+    input = get_input()
 
+    important_digits = []
+    for line in input:
+        important_digits.append(get_digits(line))
 
+    if TEST:
+        print(important_digits)
+
+    total = 0
+    for number in important_digits:
+        total+= int(number)
+
+    print(total)
     print("done")
 
 

fd8d20dbb441f9d2d6a56175dff8f5416724e32a
Aditya Revat <adi.revat@cmrsurgical.com>
2023-12-04 16:36:35 +0000

    Add regex to find useful symbols

--- 2023/Day3/Day3.py
+++ 2023/Day3/Day3.py
@@ -1,6 +1,7 @@
 try:
     import logging
     import os
+    import re
 
 except:
     print("Imports failed")
@@ -20,20 +21,35 @@ class Schematic:
     def __init__(self, sch_txt):
        
         self.board = []#[y coord(inc as you go down)][x coord ]
+        self.symbols = {}
+
 
         self.parse_input(sch_txt)
-        self.symbols = []
+        
 
 
     def parse_input(self, text):
+        symbol_buffer = []
         for i, line in enumerate(text):
-                self.board.append(line)
+            self.board.append(line)
 
-                #use regex to find symbols
-                #for each symbol, get x and y and type
-                #append to big list of symbols
+            #use regex to find symbols
+            pattern = re.compile(r"(?:[^\.0-9\n])")
+            regex_symbols = re.findall(pattern, line)
 
+            #for each symbol, get x and y and type
+            for symbol_text in regex_symbols:
+                symbol_buffer.append(self.record_symbol(symbol_text, line = i+1))
+            
+            #append to big dict of symbols (list of symbols - value) on a line (key)
+            self.symbols[i+1] = symbol_buffer
+            symbol_buffer.clear()
 
+    def record_symbol(self, symbol_text, line):
+        print(symbol_text)
+        #symbol = Symbol(symbol, x,yline)
+
+        #return symbol
 
     def __repr__(self):
         str = "TODO rpr"
@@ -45,17 +61,18 @@ class Schematic:
 
 
 class Symbol:
-    def __init__(self, symbol, x, y):
+    def __init__(self, symbol, x, y, line):
         self.x = x
         self.y = y
         self.name = symbol
+        self.line = line
 
     def __repr__(self):
-        str = "{} (Y{}:,X:{})".format(self.name, self.y, self.x)
+        str = "Line:{}, {} (Y{}:,X:{})".format(self.line, self.name, self.y, self.x)
         return(str)
     
     def __str__(self):
-        str = "{} (Y{}:,X:{})".format(self.name, self.y, self.x)
+        str = "Line{}, {} (Y{}:,X:{})".format(self.line, self.name, self.y, self.x)
         return(str)
 
 
@@ -72,7 +89,8 @@ def get_input():
     return input
 
 def main():
-
+    input = get_input()
+    schematic = Schematic(input)
 
     print("done")
 

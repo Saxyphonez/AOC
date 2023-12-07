d9a7fd7fb9b1457ab21577a627249eabe2e3cae8
Saxyphonez <89347999+Saxyphonez@users.noreply.github.com>
2023-12-07 18:14:45 +0000

    DAY 7wip

--- 2023/Day7/Day7.py
+++ 2023/Day7/Day7.py
@@ -2,6 +2,7 @@ try:
     import logging
     import os
     import timeit
+    from enum import Enum
 
 except:
     print("Imports failed")
@@ -12,6 +13,42 @@ if TEST:
     input_filename = "test_input.txt"
 else:
     input_filename = "input.txt"
+
+class HandType(Enum):
+    NONE = 7
+
+    FIVE_KIND  = 6
+    FOUR_KIND  = 5
+    THREE_KIND  = 4
+    FULL_HOUSE  = 3
+    TWO_PAIR  = 2
+    ONE_PAIR  = 1
+    HIGH_CARD  = 0
+
+class Hand():
+    def __init__(self, raw_data):
+        self.raw_hand = raw_data.split(" ")[0].strip()
+       
+
+        self.bid = raw_data.split(" ")[1].strip()
+        self.bid_int= int(self.bid)
+
+        self.hand = list(self.raw_hand)
+
+
+    def get_HandType(self):
+        buf = HandType
+
+        return 
+
+
+    def __repr__(self):
+        str = "Hand {} Bid {}".format(self.raw_hand, self.bid_int)
+        return(str)
+    
+    def __str__(self):
+        return(self.__repr__())
+
 
 def get_input():
     input = []
@@ -26,17 +63,25 @@ def get_input():
     return input
 
 def main():
+    #parse input
+    raw_input = get_input()
 
+    #create hands
+    hands = []
+    for line in raw_input:
+        hands.append(Hand(line))
+
+    #perform the comparisons
 
     print("done")
 
 
 if __name__ == "__main__":
     try:
-        total_time = timeit.timeit('main', number=1, globals=globals())
-        #main()
+        #total_time = timeit.timeit('main', number=1, globals=globals())
+        main()
 
     except KeyboardInterrupt:
         print("KB interrupt detected")
 
-    print('Average time: {} usec'.format((total_time/1)*1e6))
+    #print('Average time: {} usec'.format((total_time/1)*1e6))

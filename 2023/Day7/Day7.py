e7cebf0eec43163f4531c11ff07cc56edb365b58
Adi Revat <adirevat@Adi-MacBook-Air>
2023-12-07 23:05:35 +0000

    Completed day7 part 1

--- 2023/Day7/Day7.py
+++ 2023/Day7/Day7.py
@@ -4,14 +4,18 @@ try:
     import timeit
     from enum import Enum
     import re
+    #from sort import *
 
 except:
     print("Imports failed")
 
-TEST = True
+TEST = not True
+UNIT_TEST = False
 
 if TEST:
     input_filename = "test_input.txt"
+    #input_filename = "unit_test.txt"
+    output_filename = "sorted.txt"
 else:
     input_filename = "input.txt"
 
@@ -21,8 +25,6 @@ for i in range (2,10):
     CARDS.append(str(i))
 
 CARDS.extend(list(FACE_CARDS))
-
-
 
 class HandType(Enum):
     NONE = 7 #just in case
@@ -40,13 +42,21 @@ class Hand():
     def __init__(self, raw_data):
         self.raw_hand = raw_data.split(" ")[0].strip()
        
-
         self.bid = raw_data.split(" ")[1].strip()
         self.bid_int= int(self.bid)
 
-        self.hand = list(self.raw_hand)
-        self.get_HandType(self.hand)
+        self.hand_txt = list(self.raw_hand)
+        self.hand_ints = self.gen_hand_ints(self.hand_txt)
+        self.type = HandType.NONE
 
+        self.get_HandType(self.hand_txt)
+
+    def gen_hand_ints(self, hand_txt):
+        hand_int = []
+        for card in hand_txt:
+            hand_int.append(CARDS.index(card))
+
+        return hand_int
 
     def get_HandType(self, hand_txt):
         output = {}
@@ -64,7 +74,6 @@ class Hand():
             self.type = HandType.FOUR_KIND
 
         elif max_cnt == 3:
-            print(min(output, key = output.get))
             if output[min(output, key = output.get)] == 1:
                 self.type = HandType.THREE_KIND
             else:
@@ -82,7 +91,6 @@ class Hand():
             self.type = HandType.HIGH_CARD
 
                 
-
     def __repr__(self):
         str = "Hand {} Bid {} Type {}".format(self.raw_hand, self.bid_int, self.type)
         return(str)
@@ -103,17 +111,136 @@ def get_input():
     input = [line.strip() for line in input]
     return input
 
+def quick_sort(hands_lst, func_i = 0):
+    
+    if len(hands_lst) <= 1:
+        return hands_lst
+
+    elif len(hands_lst) == 2:
+        left = []
+        right = []
+        other_hand = hands_lst[0]
+        pivot = hands_lst[1]
+
+        for i in range(0,len(pivot.hand_ints)):
+            #print("{} vs {}".format(other_hand.hand_ints[i], pivot.hand_ints[i]))
+
+            if other_hand.hand_ints[i] > pivot.hand_ints[i]:
+                right.append(other_hand)
+                left.append(pivot)
+                break
+
+            elif other_hand.hand_ints[i] < pivot.hand_ints[i]:
+                left.append(other_hand)
+                right.append(pivot)
+                break
+
+            elif other_hand.hand_ints[i] == pivot.hand_ints[i]:
+                continue
+
+            else:
+                print("Identical")
+
+        return left + right
+            
+
+    else:
+        left = []
+        middle = []
+        right = []
+        pivot_hand = hands_lst[len(hands_lst)//2]
+        pivot = pivot_hand.hand_ints
+
+        for hand in hands_lst:
+            #print(hand)
+            for i in range(0,len(pivot)):
+                #("{} vs {}".format(hand.hand_ints[i], pivot[i]))
+                if hand.hand_ints[i] > pivot[i]:
+                    right.append(hand)
+                    break
+
+                elif hand.hand_ints[i] < pivot[i]:
+                    left.append(hand)
+                    break
+
+                elif hand.hand_ints[i] == pivot[i]:
+                    continue
+
+
+        return quick_sort(left) + [pivot_hand] + quick_sort(right)
+
+
 def main():
     #parse input
     raw_input = get_input()
 
     #create hands
     hands = []
+    hands_five = []
+    hands_four = []
+    hands_three = []
+    hands_full = []
+    hands_two = []
+    hands_one = []
+    hands_high = []
+
+    
+
     for line in raw_input:
-        hands.append(Hand(line))
+        hand_buf = Hand(line)
+        #hands.append(hand_buf)
+
+        match hand_buf.type:
+            case HandType.FIVE_KIND:
+                hands_five.append(hand_buf)
+
+            case HandType.FOUR_KIND:
+                hands_four.append(hand_buf)
+
+            case HandType.THREE_KIND:
+                hands_three.append(hand_buf)
+
+            case HandType.FULL_HOUSE:
+                hands_full.append(hand_buf)
+
+            case HandType.TWO_PAIR:
+                hands_two.append(hand_buf)
+
+            case HandType.ONE_PAIR:
+                hands_one.append(hand_buf)
+
+            case HandType.HIGH_CARD:
+                hands_high.append(hand_buf)
 
     #perform the comparisons
+    list_of_list = [hands_five, hands_four, hands_full, 
+                    hands_three, hands_two, hands_one, 
+                    hands_high]
+    list_of_list.reverse()
 
+    sorted = []
+    for group in list_of_list:
+        if len(group) == 0:
+            continue
+        else:
+            sorted.extend(quick_sort(group, 0))
+
+    if UNIT_TEST:
+        output_filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), output_filename)
+        with open(output_filepath,'w') as f:
+            for item in sorted:
+            # write each item on a new line
+                f.write("%s\n" % item)
+        return
+
+    #print(sorted)
+    total = 0
+
+    for i, hand in enumerate(sorted):
+        #print("Bid {} * {}".format(hand.bid_int, i+1))
+        total += hand.bid_int * (i+1)
+
+    print(total)
     print("done")
 
 

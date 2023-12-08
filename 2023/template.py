3589be5d22258244c6a2815681d29bfeaa41aa2e
Saxyphonez <89347999+Saxyphonez@users.noreply.github.com>
2023-12-08 17:08:07 +0000

    Comment out timeit as never used. Add template object class to template file

--- 2023/template.py
+++ 2023/template.py
@@ -13,6 +13,20 @@ if TEST:
 else:
     input_filename = "input.txt"
 
+class ClassName:
+    def __init__(self ):
+        self.name = "TODO"
+        pass
+
+
+    def __repr__(self):
+        str = "{}".format(self.name)
+        return(str)
+    
+    def __str__(self):
+        return(self.__repr__())
+    
+
 def get_input():
     input = []
 
@@ -33,10 +47,10 @@ def main():
 
 if __name__ == "__main__":
     try:
-        total_time = timeit.timeit('main', number=1, globals=globals())
-        #main()
+        #total_time = timeit.timeit('main', number=1, globals=globals())
+        main()
 
     except KeyboardInterrupt:
         print("KB interrupt detected")
 
-    print('Average time: {} usec'.format((total_time/1)*1e6))
+    #print('Average time: {} usec'.format((total_time/1)*1e6))
